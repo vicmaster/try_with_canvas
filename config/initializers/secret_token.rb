3db3717c014c958377d1d991a8d546823acb5cf8
Victor Velazquez <velazquezgaspar16@hotmail.com>
2013-07-04 00:36:59 -0500

    Mega commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryWithCanvas::Application.config.secret_token = '1bca1d180958ac02117ab2591d832eb68801ebd62e3934896cddd9d9834c66e7d5c3d2fc2c489f68373c0225e8e1940519d7f6c7313a01121d5494dc4672bf29'
